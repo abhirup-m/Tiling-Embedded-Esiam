6737106dffa04f6056374efa3fa540f54aec62bf
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-06-11 01:01:47 +0530

    --no-message

--- numerics/source/plotting.jl
+++ numerics/source/plotting.jl
@@ -1,14 +1,13 @@
-import PythonPlot
-const plt = PythonPlot
-using LaTeXStrings
-using Measures
-# DISCRETE_CGRAD = cgrad(:seaborn_rocket_gradient, 2, categorical=true)
+using PyPlot
 
-function plotHeatmaps(axes, results_arr, x_arr, y_arr)
-    for (ax, result) in zip(axes, results_arr)
-        reshaped_result = reshape(result, (length(x_arr), length(y_arr)))
-        image = ax.imshow(reshaped_result, aspect=1, origin="lower")
-        fig.colorbar(image, cax=ax)
+function plotHeatmaps(x_arr, collatedResults, saveName)
+    fig, axes = PyPlot.subplots(size(collatedResults)...)
+    for (i, results_arr) in enumerate(collatedResults)
+        for (ax, result) in zip(axes[i], results_arr)
+            reshaped_result = reshape(result, (length(x_arr), length(y_arr)))
+            image = ax.imshow(reshaped_result, aspect=1, origin="lower")
+            fig.colorbar(image, cax=ax)
+        end
     end
-    return fig, axes
 end
+
